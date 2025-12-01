de9590849892fd1029a20048e7ca7163e0b9ff16
Mariana Beatriz <marianabeatrizifpe@gmail.com>
2025-12-01 08:19:57 -0300

    Explicação do porquê da consulta que gerou uma etapa da T2
    Added comments explaining the purpose of the query and added additional queries to check for null values in 'tipo_de_poste' and 'barramento'.

--- Projeto Integralização/elt/consultas/c1.sql
+++ Projeto Integralização/elt/consultas/c1.sql
@@ -1,1 +1,6 @@
-SELECT* FROM postes_sem_duplicatas_sem_id where localizacao is null
+-- Essa consulta foi feita para a consulta de localizacao nula e gerar uma etapa na T2
+-- de substituição dos null por "NAO INFORMADO" 
+-- A persistência de valores nulos foi percebida na consulta de padronização, para gerar a T3, então fizemos essa consulta para alterar a T2
+SELECT * FROM postes_sem_duplicatas_sem_id where localizacao is null;
+SELECT * FROM postes_sem_duplicatas_sem_id where tipo_de_poste is null;
+SELECT * FROM postes_sem_duplicatas_sem_id where barramento is null
